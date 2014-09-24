36cebde278a1b4ede7068e8b2a869f2bfcf47baf
Dreae <penitenttangentt@gmail.com>
2014-09-24 17:13:01 -0400

    Added file list

--- src/com/nidhoggr/NidhoggrActor.scala
+++ src/com/nidhoggr/NidhoggrActor.scala
@@ -3,6 +3,7 @@ package com.nidhoggr
 import akka.actor.Actor
 import spray.http.MediaTypes.`application/json`
 import spray.routing._
+import java.io.File
 
 class NidhoggrActor extends Actor with HttpService {
   val myRoute =
@@ -16,7 +17,10 @@ class NidhoggrActor extends Actor with HttpService {
     path("mrcbin" / "list") {
       get {
         respondWithMediaType(`application/json`) {
-          complete("Hello foo")
+          complete {
+            val f = new File("/home/calvr")
+            "[" + f.listFiles.filter(_.isFile).map(_.getName).mkString(", ") + "]"
+          }
         }
       }
     }

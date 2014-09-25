57b9a19d19ab2a5c722eb95a7b6dc7a82321d639
Dreae <penitenttangentt@gmail.com>
2014-09-25 14:48:30 -0400

    Added config, fixed file list

--- src/com/nidhoggr/NidhoggrActor.scala
+++ src/com/nidhoggr/NidhoggrActor.scala
@@ -3,6 +3,8 @@ package com.nidhoggr
 import akka.actor.Actor
 import spray.http.MediaTypes.`application/json`
 import spray.routing._
+import spray.json._
+import spray.json.DefaultJsonProtocol._
 import java.io.File
 
 class NidhoggrActor extends Actor with HttpService {
@@ -18,8 +20,14 @@ class NidhoggrActor extends Actor with HttpService {
       get {
         respondWithMediaType(`application/json`) {
           complete {
-            val f = new File("/home/calvr")
-            "[" + f.listFiles.filter(_.isFile).map(_.getName).mkString(", ") + "]"
+            val f = new File(context.system.settings.config.getString("nidhoggr.mrcdir"))
+            val kvs = {
+              f.listFiles.filter(_.isDirectory)
+                .map((file : File) => (file.getName, file.listFiles))
+                .filter({ case (name : String, null) => false case default => true })
+                .map({ case (name : String, files : Array[File]) => (name, files.filter(_.isFile).map(_.getName))})
+            }
+            Map(kvs:_*).toJson.prettyPrint
           }
         }
       }

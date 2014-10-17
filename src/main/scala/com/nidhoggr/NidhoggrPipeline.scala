d0ab2fc24fe5081b9a1ce4071cd5c2c896ea4a68
quinn <qjones1@mix.wvu.edu>
2014-10-17 13:50:22 -0400

    Added some stuff

--- src/main/scala/com/nidhoggr/NidhoggrPipeline.scala
+++ src/main/scala/com/nidhoggr/NidhoggrPipeline.scala
@@ -3,6 +3,21 @@ package com.nidhoggr
 import com.nidhoggr.NidhoggrPipeline.{PipelineResult, PipelineMsg, PipelineFunction}
 
 object NidhoggrPipeline {
+
+  def apply():NidhoggrPipeline = {
+    new NidhoggrPipeline(List(expandInput(_), edgeDetection(_), axonOptimization(_), earthMoversDistance(_)))
+  }
+
+  //TODO all of those down there
+
+  def expandInput(msg: PipelineMsg): PipelineMsg = ???
+
+  def edgeDetection(msg: PipelineMsg): PipelineMsg = ???
+
+  def axonOptimization(msg: PipelineMsg): PipelineMsg = ???
+
+  def earthMoversDistance(msg: PipelineMsg): PipelineMsg = ???
+
   type Task = (String, String) //Why is this Strings?
   type Image = List[List[Int]]
   type PipelineMsg = (Option[(Image, Image)], Task)
@@ -25,21 +40,4 @@ class NidhoggrPipeline private (pipe: List[PipelineFunction]) {
         case e:AccuracyBelowThresholdException => ???  //TODO put T back on the stack
       }
   }
-}
-
-object AxonPipeline extends NidhoggrPipeline{
-  def apply() = {
-    val functions = List(expandInput(_),edgeDetection(_),axonOptimization(_),earthMoversDistance(_))
-    new NidhoggrPipeline(functions)
-  }
-
-  //TODO all of those down there
-
-  def expandInput(msg: PipelineMsg): PipelineMsg = ???
-
-  def edgeDetection(msg: PipelineMsg): PipelineMsg = ???
-
-  def axonOptimization(msg: PipelineMsg): PipelineMsg = ???
-
-  def earthMoversDistance(msg: PipelineMsg): PipelineMsg = ???
 }

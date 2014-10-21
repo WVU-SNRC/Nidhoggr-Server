6b5cf7f3a70578dd0b2c3da618449f12c211a282
quinn <qjones1@mix.wvu.edu>
2014-10-21 13:33:21 -0400

    Added some stuff

--- src/main/scala/com/nidhoggr/NidhoggrPipeline.scala
+++ src/main/scala/com/nidhoggr/NidhoggrPipeline.scala
@@ -7,12 +7,19 @@ case class AccuracyBelowThresholdException(task: Task) extends RuntimeException
 object NidhoggrPipeline {
 
   def apply():NidhoggrPipeline = {
-    new NidhoggrPipeline(List(expandInput(_), edgeDetection(_), axonOptimization(_), earthMoversDistance(_)))
+    new NidhoggrPipeline(List(expandInput(_), edgeDetection(_), axonOptimization(_), binSimilarityDistance(_)))
   }
 
   //TODO all of those down there
 
-  def normalize(msg:PipelineMsg): PipelineMsg = ???
+  def normalize(msg:PipelineMsg): PipelineMsg = {
+    def normalizer(img:Image) = {
+      val sum = img.flatten.foldLeft(0.0)(_+_)
+      img.map(_.map(_/sum))
+    }
+
+    (Some((normalizer(msg._1.get._1),normalizer(msg._1.get._2))),msg._2)
+  }
 
   def expandInput(msg: PipelineMsg): PipelineMsg = ???
 
@@ -20,21 +27,26 @@ object NidhoggrPipeline {
 
   def axonOptimization(msg: PipelineMsg): PipelineMsg = ???
 
-  def earthMoversDistance(msg: PipelineMsg): PipelineMsg = {
-    def measure(msg: PipelineMsg): Double = {
-      val P = msg._1.get._1
-      val IsubP = (for (i <- List.range(0, P.length)) yield i).map(a=> (for (j <- List.range(0, P(0).length)) yield j).map(b=>(a,b)))
-      val IsubQ = List((0,0)) //TODO Complicate task by adding a reference to the producing countour image
-      val J = IsubP.zip(IsubQ)
-      3.4  //I think we all know this is a placeholder
+  def binSimilarityDistance(msg: PipelineMsg): PipelineMsg = {
+    def measure(ref:Image,obs:Image):Double = {
+      val Ip = (for (i <- List.range(0, ref.length)) yield i).map(a=> (for (j <- List.range(0, ref(0).length)) yield j).map(b=>(a,b))).flatten
+      val Iq = (for (i <- List.range(0, obs.length)) yield i).map(a=> (for (j <- List.range(0, obs(0).length)) yield j).map(b=>(a,b))).flatten
+      val J = for {p <- Ip;q<-Iq} yield(p,q)
+      type Index = ((Int,Int),(Int,Int))
+      def computeSimilarityMatrix(indexer:List[Index],ref:Image,obs:Image):List[Double] = indexer match{ //TODO discuss whether we want to replace this with flow for emd
+        case ((ref1,ref2),(obs1,obs2))::is => List((ref(ref1)(ref2) - obs(obs1)(obs2))) ++ computeSimilarityMatrix(is,ref,obs)
+        case Nil => Nil
+      }
+
     }
-    val x = measure(normalize(msg))
+    val normed = normalize(msg)
+    val x = measure(???,???) // or just make sure it comes first in the pipeline
     if (x > ???) msg
     else throw new AccuracyBelowThresholdException(msg._2.get)
   }
 
   type Task = (String, String) //Why is this Strings?
-  type Image = Array[Array[Int]]
+  type Image = Array[Array[Double]]
   type PipelineMsg = (Option[(Image, Image)], Option[Task])
   type PipelineFunction = PipelineMsg => PipelineMsg
   type PipelineResult = (Option[NidhoggrPipeline], PipelineMsg)

e13eca98ade6a01f97f7baf7be8fe32fd6a95ae1
Dreae <penitenttangentt@gmail.com>
2014-11-05 04:02:19 -0500

    Quinn, your code is gone.

--- src/main/scala/com/nidhoggr/NidhoggrPipeline.scala
+++ src/main/scala/com/nidhoggr/NidhoggrPipeline.scala
@@ -1,6 +1,6 @@
 package com.nidhoggr
 
-import com.nidhoggr.NidhoggrPipeline.{PipelineResult, Image, PipelineMsg, PipelineFunction, Task}
+import com.nidhoggr.NidhoggrPipeline.{PipelineResult, PipelineMsg, PipelineFunction, Task}
 import scala.language.implicitConversions
 import com.sksamuel.scrimage.filter.EdgeFilter
 import com.sksamuel.scrimage.{Image => SKImage}
@@ -10,7 +10,7 @@ case class AccuracyBelowThresholdException(task: Task) extends RuntimeException
 
 
 object NidhoggrPipeline {
-  implicit def Trace2Image(tr:Trace) = tr.image
+  import NidhoggrPipeline.Image._
 
   def apply():NidhoggrPipeline = {
     new NidhoggrPipeline(List(expandInput(_), edgeDetection(_), axonOptimization(_), similarityCheck(_)))
@@ -23,7 +23,7 @@ object NidhoggrPipeline {
       val sum = img.pixels.foldLeft(0.0)(_+_)
       Image(img.dimensions, img.pixels.map(_/sum))
     }
-    PipelineMsg(msg.input.map((input) => (Trace(normalizer(input._1.image), input._1.coords,input._1.state), Image2Trace(normalizer(input._2)))), msg.task)
+    PipelineMsg(msg.input.map((input) => (Trace(normalizer(input._1.image), input._1.coords), Image2Trace(normalizer(input._2.image)))), msg.task)
   }
 
   def expandInput(msg: PipelineMsg): PipelineMsg = {
@@ -32,62 +32,28 @@ object NidhoggrPipeline {
         coords <- input._1.coords;
         task <- msg.task
       ) yield {
-        val h = Vector.fill(input._2.pixels.size - (coords._1 + input._1.image.dimensions._1))(0.0d) ++ input._1.image.pixels
-        val expanded: Trace = Image(input._2.dimensions, (h ++ Vector.fill(input._2.pixels.size - h.size)(0.0d)).toArray)
+        val h = Vector.fill(input._2.image.pixels.size - (coords._1 + input._1.image.dimensions._1))(0.0d) ++ input._1.image.pixels
+        val expanded: Trace = Image(input._2.image.dimensions, (h ++ Vector.fill(input._2.image.pixels.size - h.size)(0.0d)).toArray)
         PipelineMsg(expanded, input._2, task)
       }
     res.getOrElse(msg)
   }
 
-  def edgeDetectionSK(msg: PipelineMsg): PipelineMsg = {
+  def edgeDetection(msg: PipelineMsg): PipelineMsg = {
     val res = for (
       input <- msg.input;
       task <- msg.task
     ) yield {
       val image = input._2
       val trace = input._1
-      val raster = image.filter(EdgeFilter)
+      val raster = Trace(image.image.filter(EdgeFilter), None)
       PipelineMsg(trace, raster, task)
     }
     res.getOrElse(msg)
   }
 
-  def edgeDetection(msg: PipelineMsg) = {
-    val sobelX = Array(Array(-1.0,0.0,1.0),Array(-2.0,0.0,2.0),Array(-1.0,0.0,1.0))
-    val sobelY = Array(Array(1.0,2.0,1.0),Array(0.0,0.0,0.0),Array(-1.0,-2.0,-1.0))
-    def convulution(img: Image):Image ={
-      def iter(img:ImageVirtualAccessor):Image = {
-        val indices = for{p<-1 until img.n - 1;q<-1 until img.m - 1} yield(p,q)
 
-        Image((img.m-2,img.n-1),
-          indices.par.map{case(a,b)=> {
-            val pixelX = (sobelX(0)(0)*img.get(a-1)(b-1))+(sobelX(0)(1)*img.get(a)(b-1))+
-              (sobelX(1)(0)*img.get(a-1)(b))+(sobelX(1)(1)*img.get(a)(b))+
-              (sobelX(2)(0)*img.get(a-1)(b+1))+(sobelX(2)(1)*img.get(a)(b+1))
-            val pixelY = (sobelY(0)(0)*img.get(a-1)(b-1))+(sobelY(0)(1)*img.get(a)(b-1))+
-              (sobelY(1)(0)*img.get(a-1)(b))+(sobelY(1)(1)*img.get(a)(b))+
-              (sobelY(2)(0)*img.get(a-1)(b+1))+(sobelY(2)(1)*img.get(a)(b+1))
-            Math.ceil(Math.sqrt(Math.pow(pixelX,2)+Math.pow(pixelY,2)))
-            }
-          }.toArray)
-      }
-      iter(new ImageVirtualAccessor(img))
-    }
-    PipelineMsg(getTrace(msg),convulution(getImage(msg)),getTask(msg))
-  }
-
-
-
-  def axonOptimization(msg: PipelineMsg): PipelineMsg = {
-    def estimate(state:State):State  = ???  //TODO KALMAN MAGIC
-    def sampleAndUpdate(img : ImageVirtualAccessor,state:State):Trace = {
-      def sample(img:ImageVirtualAccessor):Trace = ??? //TODO use Futures to parrellelly ray sample
-      def update(tr:Trace,state:State):Trace = ??? //TODO find the centroid of the controll points
-      update(sample(img),state)
-    }
-    val s = sampleAndUpdate(new ImageVirtualAccessor(getImage(msg)), estimate(getTrace(msg).state))
-    PipelineMsg(getTrace(msg),s,getTask(msg))
-  }
+  def axonOptimization(msg: PipelineMsg): PipelineMsg = ???
 
 
 
@@ -101,10 +67,9 @@ object NidhoggrPipeline {
   }
 
   case class Task(cell: String, file: String)
-  case class State(position:Position,velocity:Velocity)
   case class Position (x:Int,y:Int)
   case class Velocity (u:Double,v:Double)
-  case class Trace(image: Image, coords: Option[Coordinate],state:State)
+  case class Trace(image: Image, coords: Option[Coordinate])
   case class Image(dimensions: (Int, Int), pixels: Array[Double])
   object Image {
     implicit def Image2Trace(img: Image): Trace = Trace(img, None)
@@ -126,7 +91,7 @@ object NidhoggrPipeline {
   }
 
   def getImage(msg: PipelineMsg): Image = {
-    msg.input.get._2
+    getTrace(msg).image
   }
 
   type PipelineResult = (Option[NidhoggrPipeline], PipelineMsg)
@@ -135,16 +100,16 @@ object NidhoggrPipeline {
 
 }
 
-class ImageVirtualAccessor(data:NidhoggrPipeline.Image){
+class ImageVirtualAccessor(data: NidhoggrPipeline.Image){
   lazy val pix = data.pixels
   lazy val m = data.dimensions._1
   lazy val n = data.dimensions._2
   lazy val length = data.pixels.length
-  def get(Row:Int)(Col:Int):Double = {
+  def get(Row:Int)(Col:Int): Double = {
     pix.slice(Row*n,Row*n+n)(Col)
   }
 
-  def set(Row:Int)(Col:Int)(Pix:Double):ImageVirtualAccessor = {
+  def set(Row: Int)(Col: Int)(Pix: Double): ImageVirtualAccessor = {
     val index = Col+(Row*n)
     new ImageVirtualAccessor(new NidhoggrPipeline.Image(data.dimensions,pix.updated(index,Pix)))
   }
